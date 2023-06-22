28978b9afbd708f614222d0b4e28abc2f5c19df9
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2023-06-22 13:57:52 -0300

    Project Created

---
+++ Back-End/src/main/java/com/arthenyo/ACCatalog/AcCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.arthenyo.ACCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcCatalogApplication.class, args);
+	}
+
+}
---
+++ Back-End/src/test/java/com/arthenyo/ACCatalog/AcCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthenyo.ACCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
