19002834e202bf45ed77c33ac08bcd473c87deb7
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2023-06-26 09:13:52 -0300

    updating the excpiton

---
+++ Back-End/src/main/java/com/arthenyo/ACCatalog/servicies/exception/DateBaseException.java
@@ -0,0 +1,7 @@
+package com.arthenyo.ACCatalog.servicies.exception;
+
+public class DateBaseException extends RuntimeException{
+    public DateBaseException(String msg){
+        super(msg);
+    }
+}
